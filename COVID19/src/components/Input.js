99b81e1805a9ef1a4a844cb58f740e110105d2b3
Hector Arturo Aguirre Munguía <HectorAM@bsm.com.mx>
2021-03-22 09:02:48 -0600

    Consuming the API for application login

--- COVID19/src/components/Input.js
+++ COVID19/src/components/Input.js
@@ -3,7 +3,7 @@ import { View, TextInput } from 'react-native';
 import styles from '../utils/styles/loginAuthStyles/loginAuthStyles';
 import Icon from 'react-native-vector-icons/AntDesign';
 
-const Input = ({placeholderTxt, iconName}) => {
+const Input = ({placeholderTxt, iconName, onCh}) => {
     return(
         <View style={styles.inputContainer}>
             <Icon name={iconName} color="gray" size={25} style={{marginStart: 15, alignSelf:'center'}}/>
@@ -11,6 +11,7 @@ const Input = ({placeholderTxt, iconName}) => {
                 placeholder={placeholderTxt}
                 secureTextEntry={placeholderTxt === 'Password' && true}
                 placeholderTextColor="gray"
+                onChange={onCh}
             />
         </View>
     );
